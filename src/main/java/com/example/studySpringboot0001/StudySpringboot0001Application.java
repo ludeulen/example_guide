5f2b601eed18866a867eadded841c2e24fa38881
ludeulen <kriem546@gmail.com>
2022-12-15 17:38:44 +0900

    example_guide

---
+++ src/main/java/com/example/studySpringboot0001/StudySpringboot0001Application.java
@@ -0,0 +1,15 @@
+package com.example.studySpringboot0001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class StudySpringboot0001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringboot0001Application.class, args);
+	}
+
+}
